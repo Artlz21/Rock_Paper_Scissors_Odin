29ce3d42dc8e1ed05ed60b99ccc4fcf9360c56b6
Artlz21 <darren.lozano01@utrgv.edu>
2023-06-05 19:22:49 -0500

    very simple ui

--- Rock_Paper_Scissors.js
+++ Rock_Paper_Scissors.js
@@ -1,32 +1,11 @@
-// Game logic ie rock beats scissors, etc
-// Determine winner
-// Display winner
-
-// Get user answer
-
-const player_choice = () => {
-    flag = false;
-    let player_choice = parseInt(prompt("Select a number: 1.) Rock, 2.) Paper, or 3.) Scissors... "));
-    while(!flag){
-        if(player_choice == 1 || player_choice == 2 || player_choice == 3){
-            flag = true;
-        }
-        else{
-            console.log("Please select one of the given answers.")
-            player_choice = parseInt(prompt("Select a number: 1.) Rock, 2.) Paper, or 3.) Scissors... "));
-        }
-    }
-    
-    return(player_choice);
-}
+let player_score = 0;
+let computer_score = 0;
+let winner = document.createElement("h1");
+document.querySelector("body").appendChild(winner);
 
 const computer_choice = () => {
     const computer_choice = Math.floor(Math.random() * 3);
-    return(computer_choice + 1);
-}
-
-const convert_to_words = (answer_to_convert) => {
-    switch(answer_to_convert){
+    switch(computer_choice + 1){
         case 1:
             return("Rock");
         case 2:
@@ -36,48 +15,111 @@ const convert_to_words = (answer_to_convert) => {
     }
 }
 
+const player_Choice = (choice) => {
+    switch(choice){
+        case 1: 
+            return ("Rock");
+        case 2:
+            return ("Paper");
+        case 3:
+            return ("Scissors");
+    }
+}
+
 const compare_choices = (player_answer, computer_answer) => {
-    if(player_answer == "Rock" && computer_answer == "Rock"){
-        return "Draw...";
+    if(player_answer == computer_answer){
+        return ("Draw...");
     }
-    else if(player_answer == "Rock" && computer_answer == "Paper"){
-        return "Computer Wins!";
-    }
-    else if(player_answer == "Rock" && computer_answer == "Scissors"){
-        return "Player Wins!";
-    }
-    else if(player_answer == "Paper" && computer_answer == "Rock"){
-        return "Player Wins!";
-    }
-    else if(player_answer == "Paper" && computer_answer == "Paper"){
-        return "Draw...";
-    }
-    else if(player_answer == "Paper" && computer_answer == "Scissors"){
-        return "Computer Wins!";
-    }
-    else if(player_answer == "Scissors" && computer_answer == "Rock"){
-        return "Computer Wins!";
-    }
-    else if(player_answer == "Scissors" && computer_answer == "Paper"){
-        return "Player Wins!";
-    }
-    else if(player_answer == "Scissors" && computer_answer == "Scissors"){
-        return "Draw";
+    else if((player_answer == "Rock" && computer_answer == "Scissors") || 
+            (player_answer == "Paper" && computer_answer == "Rock") ||
+            (player_answer == "Scissors" && computer_answer == "Paper")){
+                return ("Player Wins!");
+            }
+    else{
+        return ("Computer Wins!");
     }
 }
 
-const game = () => {
-    let iterate = 0;
+const display_Result = (player, computer, result) => {
+    let player_choice_display = document.createElement("p");
+    let computer_choice_display = document.createElement("p");
+    let winner_display = document.createElement("p");
 
-    while(iterate < 5){
-        let player = convert_to_words(player_choice());
-        let computer = convert_to_words(computer_choice());
+    document.querySelector("div").appendChild(player_choice_display);
+    document.querySelector("div").appendChild(computer_choice_display);
+    document.querySelector("div").appendChild(winner_display);
 
-        console.log("Player selected: " + player);
-        console.log("Computer selected: " + computer);
-        console.log(compare_choices(player, computer));
-        iterate++;
+    player_choice_display.innerText = "Player choose: " + player;
+    computer_choice_display.innerText = "Computer choose: " + computer;
+    winner_display.innerText = result;
+}
+
+const game = (player_choice) => {
+    let player = player_Choice(player_choice);
+    let computer = computer_choice();
+    let result = compare_choices(player, computer);
+    let score;
+
+    if(result === "Player Wins!"){
+        score = 1;
+    }
+    else if(result === "Computer Wins!"){
+        score = -1;
+    }
+    else{
+        score = 0;
+    }
+
+    score_tracker(score);
+
+    display_Result(player, computer, result);
+}
+
+const create_button = (text, num) => {
+    new_button = document.createElement("button");
+    new_button.innerText = text;
+    new_button.addEventListener("click", () => game(num));
+    document.querySelector("body").appendChild(new_button);
+}
+
+
+const score_tracker = (score) => {
+
+    if(score == 1){
+        player_score += score;
+    }
+    else if(score == -1){
+        computer_score += (-1 * (score));
+    }    
+
+    let display_Result = document.createElement("div");
+    let results = document.createElement("h3");
+
+    document.querySelector("body").appendChild(display_Result);
+    display_Result.appendChild(results);
+    document.querySelector("h3").innerText = "Player Score: " + player_score + "   "
+     + "Computer Score: " + computer_score;
+
+    if(computer_score == 5 || player_score == 5){
+        if(computer_score == 5){
+            winner.innerText = "Computer scored 5 Winner!";
+            player_score = 0;
+            computer_score = 0;
+        }
+        else if(player_score == 5){
+            winner.innerText = "Player scored 5 Winner!";
+            player_score = 0;
+            computer_score = 0;
+        }
     }
 }
 
-game();
+const score_point = (score) => {
+    return (score);
+}
+
+(() => {
+    create_button("Rock", 1);
+    create_button("Paper", 2);
+    create_button("Scissors", 3);
+})();
